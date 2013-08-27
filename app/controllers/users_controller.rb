82efa3f2be4a0ef15746ad911f9be40c35b84bb7
Taylor Steinberg <tdstein@Taylors-MacBook-Pro.local>
2013-08-27 19:03:12 -0400

    Added Input Fields and CSV Download

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order(:id)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @users.to_csv }
+    end
   end
 
   # GET /users/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+	attr_accessible :firstname, :lastname, :email, :membership_id, :major, :year
+
+	def self.to_csv
+		CSV.generate do |csv|
+			csv << column_names
+			all.each do |user|
+				csv << user.attributes.values_at(*column_names)
+			end
+		end
+	end
+
 end
